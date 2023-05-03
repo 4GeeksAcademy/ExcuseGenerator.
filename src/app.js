e001b14ed3dde06cae7d2f4260ec0884fe9c7244
Antoniomorales17 <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-03 18:57:09 +0000

    firs-commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+
+window.onload = () => {
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuseGenerator();
+  });
+
+  const randomNumber = () => {
+    const random = Math.floor(Math.random() * 4);
+    return random;
+  };
+
+  const excuseGenerator = () => {
+    const who = ["My ex", "Batman", "Pikachu", "Doraemon"];
+    const action = ["orinó", "aplastó", "discutió", "golpeó"];
+    const what = [
+      "mi corazon",
+      "mi Ferrari",
+      "mi camiseta favorita",
+      "las estrellas"
+    ];
+    const when = [
+      "mientras estaba en la ducha",
+      "durante la hora de la siesta",
+      "mientras estaba en Gran Hermano",
+      "cuando me visitó Leo Messi"
+    ];
+
+    return `${who[randomNumber()]} ${action[randomNumber()]} ${
+      what[randomNumber()]
+    } ${when[randomNumber()]}`;
+  };
 };
